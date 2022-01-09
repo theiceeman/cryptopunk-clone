bdd71eeeb5ffd13d96905a5dba6e041b74f3d9f0
kelvin <okorieebube1@gmail.com>
2022-01-09 16:57:09 +0100

    feat: completed the header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import CollectionCard from "./components/CollectionCard";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <CollectionCard
+      id={0}
+      name={'Pipe Punk'}
+      traits={[{value:7}]}
+      image="https://ipfs.thirdweb.com/ipfs/bafkreiasgnibw32isqfyjnv5lhj4wxzp6izayculjlo4ampmmfla3j4tj4"/>
     </div>
   );
 }
