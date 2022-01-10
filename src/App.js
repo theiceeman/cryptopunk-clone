776d36980173f0930bc2ffe3bab4264c5eafc4b6
kelvin <okorieebube1@gmail.com>
2022-01-10 11:31:40 +0100

    feat: component for listing the punks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-
 import "./App.css";
 import Header from "./components/Header";
 import CollectionCard from "./components/CollectionCard";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0x2675eBd14474Fb3AAf118F99582e6a4F4E74b84a&order_direction=asc"
+      );
+      console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets)
+    };
+    return getMyNfts();
+  }, []);
+
   return (
     <div className="app">
       <Header />
       <CollectionCard
-      id={0}
-      name={'Pipe Punk'}
-      traits={[{value:7}]}
-      image="https://ipfs.thirdweb.com/ipfs/bafkreiasgnibw32isqfyjnv5lhj4wxzp6izayculjlo4ampmmfla3j4tj4"/>
+        id={0}
+        name={"Pipe Punk"}
+        traits={[{ value: 7 }]}
+        image="https://ipfs.thirdweb.com/ipfs/bafkreiasgnibw32isqfyjnv5lhj4wxzp6izayculjlo4ampmmfla3j4tj4"
+      />
     </div>
   );
 }
---
+++ src/components/PunkList.js
@@ -0,0 +1,1 @@
+PunkList.js
